666d389979ea6bd08ea78236d03e88b6168920b8
Shamiul <anik.savar.bd@gmail.com>
2026-02-17 17:30:11 +0900

    Rate Limit updated

--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // 100 requests per windowMs per IP
+  limit: 1000, // 100 requests per windowMs per IP
   standardHeaders: "draft-8",
   legacyHeaders: false,
   message: {
